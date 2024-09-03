5239818f47597176d179ce223ff98220da8d2396
Ferxxaa <gonzalezfernanda491@gmail.com>
2024-09-03 19:32:43 -0400

    registro

--- nuevo/src/app/register/register.page.ts
+++ nuevo/src/app/register/register.page.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
 })
-export class RegisterPage implements OnInit {
+export class RegisterPage {
+  registerForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder) {
+    this.registerForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
   }
 
+  onRegister() {
+    if (this.registerForm.valid) {
+      // Implement registration logic here
+      console.log('Form submitted:', this.registerForm.value);
+    }
+  }
 }
