476c0a8dace61140cf9bcdc7852fd48025d45b09
Ferxxaa <gonzalezfernanda491@gmail.com>
2024-09-04 10:01:56 -0400

    anuncio-animacion

--- nuevo/src/app/uber-front/uber-front.page.ts
+++ nuevo/src/app/uber-front/uber-front.page.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./uber-front.page.scss'],
 })
 export class UberFrontPage implements OnInit {
+  showOffer: boolean = true; // Para mostrar el popup de oferta
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  claimOffer() {
+    // Lógica para reclamar la oferta
+    console.log('Oferta reclamada');
+    this.closeOffer(); // Opcionalmente, cierra la oferta después de reclamarla
+  }
+
+  closeOffer() {
+    // Lógica para cerrar el popup de oferta
+    this.showOffer = false;
   }
 
 }
