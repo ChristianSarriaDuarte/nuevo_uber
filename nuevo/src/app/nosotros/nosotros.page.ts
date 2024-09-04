045db569eb9e342dddb7080d2e14a5cf3f5c22fc
Ferxxaa <gonzalezfernanda491@gmail.com>
2024-09-03 21:29:49 -0400

    nosotros2

--- nuevo/src/app/nosotros/nosotros.page.ts
+++ nuevo/src/app/nosotros/nosotros.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-nosotros',
@@ -9,7 +10,16 @@ export class NosotrosPage implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      // Aquí puedes manejar el envío del formulario, como mostrar un mensaje de éxito
+      console.log('Formulario enviado:', form.value);
+    } else {
+      // Aquí puedes manejar errores de validación
+      console.log('Formulario inválido');
+    }
   }
 
 }
